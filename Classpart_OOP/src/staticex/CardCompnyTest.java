313db5ac1557b0001df467ec90852de380d9ce7c
hayoonpark <gkdbs0120@gmail.com>
2021-07-27 21:35:15 +0900

    static practice

---
+++ Classpart_OOP/src/staticex/CardCompnyTest.java
@@ -0,0 +1,16 @@
+package staticex;
+
+public class CardCompnyTest {
+
+	public static void main(String[] args) {
+		CardCompany company = CardCompany.getInstance();
+		
+		Card myCard = company.createCard();
+		Card yourCard = company.createCard();
+		
+		System.out.println(myCard.getCardNumber());
+		System.out.println(yourCard.getCardNumber());
+
+	}
+
+}
