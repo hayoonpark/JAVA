f0117f24d723adf176854805e40f828011681364
hayoonpark <gkdbs0120@gmail.com>
2021-08-24 23:58:26 +0900

    additional commit

--- Interface/src/bookshelf/BookShelf.java
+++ Interface/src/bookshelf/BookShelf.java
@@ -1,5 +1,23 @@
 package bookshelf;
 
-public class BookShelf {
+public class BookShelf extends Shelf implements Queue {
+
+	@Override
+	public void enQueu(String title) {
+		shelf.add(title);
+		
+	}
+
+	@Override
+	public String deQueue() {
+		// TODO Auto-generated method stub
+		return shelf.remove(0);
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return getCount();
+	}
 
 }
